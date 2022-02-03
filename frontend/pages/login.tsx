71efe3be44a648a2cff542534c90093106ac7ebc
Abdulla Alkhulaqui <60021814+Abdulla-Alkhulaqui@users.noreply.github.com>
2022-02-03 23:57:57 +0100

    basic frontend setup with login page creation

---
+++ frontend/pages/login.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import Image from "next/image";
+const Login: NextPage = () => {
+  return (
+    <div className="login-container">
+      <div className="bc-image-container">
+        <Image
+          src="/elte.jpg"
+          objectFit="cover"
+          layout="fill"
+          priority={true}
+        />
+      </div>
+      <div className="login-form-container">
+        <form className="login-form">
+          <div className="login-form-title">
+            <h2>Login with your Neptun</h2>
+          </div>
+          <label>Neptun</label>
+          <input type="text" placeholder="Write your neptun code..." />
+          <label>Password</label>
+          <input type="text" placeholder="Write your password..." />
+          <input id="login-btn" type="submit" value="Login" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;

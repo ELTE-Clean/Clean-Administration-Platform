30719b46303d4df6c199db28b8811de06337fad8
Asseel Al-Mahdawi <asseel.mahdawi@gmail.com>
2022-02-11 18:27:13 +0100

    Fixed task component bug

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from "next";
+import Task from "../components/task";
 import AddSectionForm from "../components/AddSectionForm";
 import Section from "../components/section";
-import Task from "../components/task";
+
 const Home: NextPage = () => {
   return (
     <div>
-      { <Section sectionName="Homework" />}
-      {/* <Task /> */}
-      {/*<AddSectionForm />*/}
+      {/* <Section sectionName="Homework" /> */}
+      <Task />
+      {/* <AddSectionForm /> */}
     </div>
   );
 };

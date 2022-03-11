491d7a6cb091de971bd7bad2601bb7988bded97c
Abdulla Alkhulaqui <60021814+Abdulla-Alkhulaqui@users.noreply.github.com>
2022-03-11 16:28:26 +0100

    Script concurrency (#46)
    * solving concurrency bug
    
    * feat: added conccurency to the script
    
    * removed Clean System Files

--- scripts/CorrectTest.py
+++ scripts/CorrectTest.py
@@ -6,6 +6,7 @@ import yaml
 import re
 import argparse
 import time
+import threading
 
 
 class CorrectTest:
@@ -19,14 +20,23 @@ class CorrectTest:
             self.student_files = yamlData["student_files"]
             self.teacher_result = self.test_file(self.teacher_file)
             self.students_results = {}
+            # threads = []
+            # for student_file in self.student_files:
+            #     t = threading.Thread(
+            #         target=self.assign_student_result, args=[student_file])
+            #     t.start()
+            #     threads.append(t)
+
+            # for thread in threads:
+            #     thread.join()
 
             with ThreadPoolExecutor(max_workers=threads_number) as executor:
+                for student_file in self.student_files:
+                    executor.submit(self.assign_student_result, student_file)
 
-                # for student_file in self.student_files:
-                #     executor.submit(self.assign_student_result, student_file)
-
-                executor.map(lambda student_file:  self.assign_student_result(
-                    student_file), self.student_files)
+                # results = executor.map(
+                #     self.assign_student_result, self.student_files)
+                # print([result.result() for result in results])
 
             self.display_diff(self.teacher_result, self.students_results)
             print(self.students_results)
@@ -34,11 +44,12 @@ class CorrectTest:
                 f'{len(self.student_files)} files in {datetime.datetime.now() - begin_time}')
 
     def assign_student_result(self, student_file):
-        time.sleep(1)
+        print("file: ", student_file)
         prior_path, file_name_without_icl, ignore = self.get_student_name_from_path(
             student_file)
         self.students_results[file_name_without_icl] = self.test_file(
             student_file)
+        # return self.test_file(student_file)
 
     def removeLastLineFromFile(self, file_path):
         with open(file_path, 'r+') as read_obj:
@@ -71,7 +82,8 @@ class CorrectTest:
                 with open(file_path, 'a') as write_obj:
                     write_obj.write(f'Start = {function_name} {test}\n')
 
-                command = f'clm -I {prior_path} {file_name_without_icl}' if prior_path != "" else f'clm -I ./ {file_name_without_icl}'
+                print("Imported ", f'Start = {function_name} {test}\n')
+                command = f'clm -I  {prior_path} {file_name_without_icl} -o {file_name_without_icl}.out' if prior_path != "" else f'clm -I ./ {file_name_without_icl} -o {file_name_without_icl}.out'
                 try:
                     log = subprocess.check_output(
                         command, shell=True, stderr=subprocess.STDOUT).decode('utf-8')
@@ -79,7 +91,7 @@ class CorrectTest:
                     log = "\n".join(log.split("\n")[:3])
                     # print(log)
                     if log == pass_str:
-                        command = "./a.out"
+                        command = f"./{file_name_without_icl}.out"
                         log = subprocess.check_output(
                             command, shell=False, stderr=subprocess.STDOUT).decode('utf-8')
 
@@ -88,6 +100,7 @@ class CorrectTest:
                         function_tests[test] = test_result
 
                     else:
+                        print(log)
                         raise subprocess.CalledProcessError(
                             returncode=1, cmd=command)
                 except subprocess.CalledProcessError:
@@ -95,7 +108,7 @@ class CorrectTest:
 
                 self.removeLastLineFromFile(file_path)
             file_result[function_name] = function_tests
-
+        print("ENDD")
         return file_result
 
     def get_student_name_from_path(self, file_path):

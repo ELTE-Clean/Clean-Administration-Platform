6b692da651535df2c98029e92100b4e6773f3dfb
Asseel Al-Mahdawi <asseel.mahdawi@gmail.com>
2022-05-27 11:08:02 +0200

    Connected Admin endpoints

--- cleanDB/scripts/clean_db.sql
+++ cleanDB/scripts/clean_db.sql
@@ -123,8 +123,6 @@ ALTER SEQUENCE public.taskSeq OWNER TO postgres;
 --
 
 CREATE TABLE public.tasks (
-<<<<<<< HEAD
-<<<<<<< HEAD
     taskID          INT            DEFAULT nextval('taskSeq'),
     taskName        VARCHAR(50)    NOT NULL,
     sectionID       INT            REFERENCES  sections(sectionID)  NOT NULL,
@@ -133,25 +131,8 @@ CREATE TABLE public.tasks (
     solution        VARCHAR(5000)  NOT NULL, -- Task solution to run the script on!
     testQuestions   VARCHAR(1000), -- Test cases for the configuration
     max             INT            NOT NULL,
-<<<<<<< HEAD
     expiryDate      DATE,
     expiryTime      TIME,
-=======
-=======
-=======
->>>>>>> 0183be8 (Rebasing...)
-    taskID      INT            DEFAULT nextval('taskSeq'),
-    taskName    VARCHAR(50)    NOT NULL,
-    sectionID   INT            REFERENCES  sections(sectionID)  NOT NULL,
-    groupID     INT            REFERENCES  groups(groupID)      NOT NULL,
-    description VARCHAR(5000)  NULL, -- Task description (File/Text)
-    solution    VARCHAR(5000)  NULL, -- Task solution to run the script on!
-    max         INT            NULL,
-<<<<<<< HEAD
->>>>>>> 4a38981 (Make task solution, description, max grade optional)
->>>>>>> 9d3878b (Rebasing...)
-=======
->>>>>>> 0183be8 (Rebasing...)
     PRIMARY KEY (taskID)
 );
 
@@ -188,9 +169,9 @@ ALTER TABLE public.grades OWNER TO postgres;
 --
 
 CREATE TABLE public.user_to_group (
-    neptun      VARCHAR(6)     REFERENCES users(neptun)    NOT NULL,
-    groupID     INT            REFERENCES groups(groupID)  NOT NULL,
-    PRIMARY KEY (neptun, groupID)
+    userID      INT             REFERENCES users(userID)    NOT NULL,
+    groupID     INT             REFERENCES groups(groupID)  NOT NULL,
+    PRIMARY KEY (userID, groupID)
 );
 
 
@@ -217,7 +198,6 @@ INSERT INTO sections (sectionName, groupID) VALUES ('Homework', 2);
 INSERT INTO sections (sectionName, groupID) VALUES ('Midterm', 4);
 INSERT INTO sections (sectionName, groupID) VALUES ('Endterm', 2);
 
-<<<<<<< HEAD
 
 INSERT INTO user_to_group (userID, groupID) VALUES (1, 1);
 INSERT INTO user_to_group (userID, groupID) VALUES (2, 2);
@@ -234,14 +214,10 @@ INSERT INTO user_to_group (userID, groupID) VALUES (6, 4);
 ---------------------- Teacher tasks and files Dumb data
 INSERT INTO tasks (taskName, sectionID, description, solution, testQuestions,  max) VALUES ('Homework 1', 1, 'desc', 'module teacher
 import StdEnv 
-
-
 addInt :: Int Int -> Int
 addInt a b = a + b
-
 subInt :: Int Int -> Int
 subInt a b = a - b
-
 ',
 '
 [
@@ -258,36 +234,24 @@ subInt a b = a - b
 , 1);
 INSERT INTO tasks (taskName, sectionID, description, solution, max) VALUES ('Progress Task 1', 2, 'desc', 'module teacher
 import StdEnv 
-
-
 addInt :: Int Int -> Int
 addInt a b = a + b
-
 subInt :: Int Int -> Int
 subInt a b = a - b
-
 ', 1);
 INSERT INTO tasks (taskName, sectionID, description, solution, max) VALUES ('Homework 2', 3, 'desc', 'module teacher
 import StdEnv 
-
-
 addInt :: Int Int -> Int
 addInt a b = a + b
-
 subInt :: Int Int -> Int
 subInt a b = a - b
-
 ', 2);
 INSERT INTO tasks (taskName, sectionID, description, solution,testQuestions, max) VALUES ('Midterm', 4, 'desc', 'module teacher
 import StdEnv 
-
-
 addInt :: Int Int -> Int
 addInt a b = a + b
-
 subInt :: Int Int -> Int
 subInt a b = a - b
-
 ', 
 '
 [
@@ -304,54 +268,23 @@ subInt a b = a - b
 , 3);
 INSERT INTO tasks (taskName, sectionID, description, solution, max) VALUES ('Endterm', 5, 'desc', 'module teacher
 import StdEnv 
-
-
 addInt :: Int Int -> Int
 addInt a b = a + b
-
 subInt :: Int Int -> Int
 subInt a b = a - b
-
 ', 4);
 
 ---------------------- Students files Dumb data
 INSERT INTO grades (userID, taskID, filename, submission, grade) VALUES (1, 1, 'midterm (2).icl','import StdEnv 
-
 addInt :: Int Int -> Int
 addInt a b = a + b
-
 subInt :: Int Int -> Int
 subInt a b = a - b', 5);
 INSERT INTO grades (userID, taskID, submission, grade) VALUES (2, 1, 'import StdEnv 
-
 addInt :: Int Int -> Int
 addInt a b = a + b
-
 subInt :: Int Int -> Int
 subInt a b = a - b', 2);
 
 -- INSERT INTO grades (userID, taskID, submission, grade) VALUES (6, 4, NULL, 3);
 -- INSERT INTO grades (userID, taskID, submission, grade) VALUES (3, 5, NULL, 4);
-=======
-INSERT INTO tasks (taskName, sectionID, groupID, description, solution, max) VALUES ('Homework 1', 1, 1, 'desc', 'sol', 1);
-INSERT INTO tasks (taskName, sectionID, groupID, description, solution, max) VALUES ('Progress Task 1', 2, 1, 'desc', 'sol', 1);
-INSERT INTO tasks (taskName, sectionID, groupID, description, solution, max) VALUES ('Homework 2', 3, 2, 'desc', 'sol', 2);
-INSERT INTO tasks (taskName, sectionID, groupID, description, solution, max) VALUES ('Midterm', 4, 1, 'desc', 'sol', 3);
-INSERT INTO tasks (taskName, sectionID, groupID, description, solution, max) VALUES ('Endterm', 5, 2, 'desc', 'sol', 4);
-INSERT INTO tasks (taskName, sectionID, groupID) VALUES ('Homework 3', 3, 2);
-
-INSERT INTO grades (studentID, taskID, sectionID, submission, grade) VALUES (1, 1, 1, NULL, 5);
-INSERT INTO grades (studentID, taskID, sectionID, submission, grade) VALUES (2, 1, 3, NULL, 2);
-INSERT INTO grades (studentID, taskID, sectionID, submission, grade) VALUES (2, 4, 5, NULL, 3);
-INSERT INTO grades (studentID, taskID, sectionID, submission, grade) VALUES (3, 4, 5, NULL, 3);
-INSERT INTO grades (studentID, taskID, sectionID, submission, grade) VALUES (3, 5, 5, NULL, 4);
-
-INSERT INTO user_to_group (neptun, groupID) VALUES ('81AMIA', 1);
-INSERT INTO user_to_group (neptun, groupID) VALUES ('9YV5TX', 2);
-INSERT INTO user_to_group (neptun, groupID) VALUES ('ZEADKD', 3);
-INSERT INTO user_to_group (neptun, groupID) VALUES ('Q50YI1', 4);
-INSERT INTO user_to_group (neptun, groupID) VALUES ('B8WNS6', 1);
-INSERT INTO user_to_group (neptun, groupID) VALUES ('B8WNS6', 2);
-INSERT INTO user_to_group (neptun, groupID) VALUES ('NM82SK', 3);
-INSERT INTO user_to_group (neptun, groupID) VALUES ('NM82SK', 4);
->>>>>>> 3fceb46 (Rebasing...)
